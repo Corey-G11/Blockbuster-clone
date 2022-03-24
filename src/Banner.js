e55a638a0ecb1faf0d2c49ded44004357939fb86
Corey Falls <cgcodesjs@gmail.com>
2022-03-24 18:31:14 -0500

    Add files via upload

---
+++ src/Banner.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import "./Banner.css";
+import axios from './axios';
+import Requests from './Requests';
+
+function Banner() {
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(Requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+        fetchData();
+    }, [])
+
+    console.log(movie);
+
+    function truncate(string, n) {
+        return string?.length > n ? string.substr(0, n - 1) + '...' : string
+    };
+
+
+    return (
+        <header
+            className='banner'
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+                backgroundPosition: "center center",
+            }}
+        >
+            <div className='banner__contents'>
+                <h1 className='banner__title'>
+                    {movie?.title || movie?.name || movie?.original_name}
+                </h1>
+                <div className='Banner__buttons'>
+                    <button className='banner__button'>Play</button>
+                    <button className='banner__button'>My List</button>
+                </div>
+
+                <h1 className='banner__description'></h1>
+                {truncate(movie?.overview, 150)}
+
+
+
+            </div>
+            <div className='banner--fadeBottom' />
+        </ header>
+    )
+}
+
+export default Banner
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAbizDWECREvIjAV3Dsh4nkGrcyLahzBnk",
+    authDomain: "pro-bb-clone.firebaseapp.com",
+    projectId: "pro-bb-clone",
+    storageBucket: "pro-bb-clone.appspot.com",
+    messagingSenderId: "860188022045",
+    appId: "1:860188022045:web:5eefda6b175ca2d9c5fdf4"
+};
